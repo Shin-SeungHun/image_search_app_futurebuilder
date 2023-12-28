f323ccb561115468aaff35ac3216ae0dd1fe303f
shin-SeungHun <sng7387@gmail.com>
2023-12-29 08:16:48 +0900

    검색창 간격 조정

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -12,36 +12,41 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: '검색',
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  width: 2,
-                  color: Colors.green,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '검색',
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Colors.green,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.green,
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.cyan,
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
               ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: BorderSide(
-                  width: 2,
-                  color: Colors.green,
-                ),
-              ),
-              suffixIcon: IconButton(
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.cyan,
-                ),
-                onPressed: () {},
-              ),
-            ),
+              SizedBox(height: 24),
+            ],
           ),
-          SizedBox(height: 24),
-        ],
+        ),
       ),
     );
   }
