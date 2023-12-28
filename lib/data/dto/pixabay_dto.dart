f5ec76ca5e058d158d63e419ccf385164822643f
shin-SeungHun <sng7387@gmail.com>
2023-12-29 07:40:51 +0900

    dto에 JsonSerializable()추가

--- lib/data/dto/pixabay_dto.dart
+++ lib/data/dto/pixabay_dto.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'pixabay_dto.g.dart';
+
+@JsonSerializable()
 class PixabayDto {
   PixabayDto({
     this.total,
@@ -5,15 +10,8 @@ class PixabayDto {
     this.hits,
   });
 
-  PixabayDto.fromJson(dynamic json) {
-    total = json['total'];
-    totalHits = json['totalHits'];
-    if (json['hits'] != null) {
-      hits = [];
-      json['hits'].forEach((v) {
-        hits?.add(Hits.fromJson(v));
-      });
-    }
+  factory PixabayDto.fromJson(dynamic json) {
+    return _$PixabayDtoFromJson(json);
   }
 
   num? total;
@@ -21,16 +19,11 @@ class PixabayDto {
   List<Hits>? hits;
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['total'] = total;
-    map['totalHits'] = totalHits;
-    if (hits != null) {
-      map['hits'] = hits?.map((v) => v.toJson()).toList();
-    }
-    return map;
+    return _$PixabayDtoToJson(this);
   }
 }
 
+@JsonSerializable()
 class Hits {
   Hits({
     this.id,
@@ -57,29 +50,8 @@ class Hits {
     this.userImageURL,
   });
 
-  Hits.fromJson(dynamic json) {
-    id = json['id'];
-    pageURL = json['pageURL'];
-    type = json['type'];
-    tags = json['tags'];
-    previewURL = json['previewURL'];
-    previewWidth = json['previewWidth'];
-    previewHeight = json['previewHeight'];
-    webformatURL = json['webformatURL'];
-    webformatWidth = json['webformatWidth'];
-    webformatHeight = json['webformatHeight'];
-    largeImageURL = json['largeImageURL'];
-    imageWidth = json['imageWidth'];
-    imageHeight = json['imageHeight'];
-    imageSize = json['imageSize'];
-    views = json['views'];
-    downloads = json['downloads'];
-    collections = json['collections'];
-    likes = json['likes'];
-    comments = json['comments'];
-    userId = json['user_id'];
-    user = json['user'];
-    userImageURL = json['userImageURL'];
+  factory Hits.fromJson(dynamic json) {
+    return _$HitsFromJson(json);
   }
 
   num? id;
@@ -106,29 +78,6 @@ class Hits {
   String? userImageURL;
 
   Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['id'] = id;
-    map['pageURL'] = pageURL;
-    map['type'] = type;
-    map['tags'] = tags;
-    map['previewURL'] = previewURL;
-    map['previewWidth'] = previewWidth;
-    map['previewHeight'] = previewHeight;
-    map['webformatURL'] = webformatURL;
-    map['webformatWidth'] = webformatWidth;
-    map['webformatHeight'] = webformatHeight;
-    map['largeImageURL'] = largeImageURL;
-    map['imageWidth'] = imageWidth;
-    map['imageHeight'] = imageHeight;
-    map['imageSize'] = imageSize;
-    map['views'] = views;
-    map['downloads'] = downloads;
-    map['collections'] = collections;
-    map['likes'] = likes;
-    map['comments'] = comments;
-    map['user_id'] = userId;
-    map['user'] = user;
-    map['userImageURL'] = userImageURL;
-    return map;
+    return _$HitsToJson(this);
   }
 }
